855ea7bad2362e96de4d2d203792d6f85326b850
Nelson Menezes <nemzes@gmail.com>
2019-02-13 23:29:06 +0100

    Add plugin

--- plugin/index.php
+++ plugin/index.php
@@ -7,3 +7,43 @@ Author: Nelson Menezes
 Author URI: https://fittopage.org
 License: MIT
 */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+// create custom plugin settings menu
+add_action('admin_menu', 'sogd_create_menu');
+
+function sogd_create_menu() {
+    add_menu_page('Strikk og Drikk settings', 'S&D settings', 'administrator', 'sogd', 'sogd_settings_page');
+    add_action( 'admin_init', 'sogd_register_settings' );
+}
+
+function sogd_register_settings() {
+    //register our settings
+    register_setting( 'sogd-settings', 'sogd-festival-front-blurb' );
+    register_setting( 'sogd-settings', 'sogd-festival-enabled' );
+}
+
+function sogd_settings_page() {
+?>
+<div class="wrap">
+<h1>Your Plugin Name</h1>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'sogd-settings' ); ?>
+    <?php do_settings_sections( 'sogd-settings' ); ?>
+
+    <label>
+        <input type="checkbox" name="sogd-festival-enabled" value="<?php echo esc_attr( get_option('sogd-festival-enabled') ); ?>" />
+        Enable festival
+    </label>
+    <h2>Festival blurb</h2>
+    <?php
+        $content = get_option('sogd-festival-front-blurb');
+        wp_editor( $content, 'sogd-festival-front-blurb', $settings = array('textarea_rows'=> '10') );
+        submit_button('Save', 'primary');
+    ?>    
+</form>
+</div>
+<?php
+}
--- theme/front-page.php
+++ theme/front-page.php
@@ -1,23 +1,30 @@
 <?php get_header(); ?>
 
 	<main role="main" aria-label="Content">
-    <section
-      class="festival-hero"
-      style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/img/sample-festival-splash.jpg)"
-    >
+    <section class="festival-hero">
+      <div
+        class="festival-hero__back"
+        style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/img/sample-festival-splash.jpg)"
+      >
+        <div class="layout-clamp">
+          <div class="festival-hero__heading">
+            <h1>Stavanger Strikkefestival</h1>
+          </div>
+        </div>
+      </div>
       <div class="layout-clamp">
         <div class="festival-hero__content">
-          <h1>Stavanger Strikkefestival</h1>
+          <?php echo wp_kses_post( get_option( 'sogd-festival-front-blurb' ) ) ?>
         </div>
       </div>
     </section>
 
     <section>
-			<?php get_template_part( 'loop' ); ?>
+      <?php get_template_part( 'loop' ); ?>
 
-			<?php get_template_part( 'pagination' ); ?>
+      <?php get_template_part( 'pagination' ); ?>
 
-		</section>
-	</main>
+    </section>
+  </main>
 
 <?php get_footer(); ?>
