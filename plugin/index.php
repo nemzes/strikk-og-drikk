dee041ef1e1f0ed24169abfd71f31d8a8024d1b7
Nelson Menezes <nemzes@gmail.com>
2019-02-16 15:35:58 +0100

    Add festival category fields

--- plugin/index.php
+++ plugin/index.php
@@ -8,42 +8,86 @@ Author URI: https://fittopage.org
 License: MIT
 */
 
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined('ABSPATH') or die('No script kiddies please!');
 
 // create custom plugin settings menu
 add_action('admin_menu', 'sogd_create_menu');
 
-function sogd_create_menu() {
+function sogd_create_menu()
+{
     add_menu_page('Strikk og Drikk settings', 'S&D settings', 'administrator', 'sogd', 'sogd_settings_page');
-    add_action( 'admin_init', 'sogd_register_settings' );
+    add_action('admin_init', 'sogd_register_settings');
 }
 
-function sogd_register_settings() {
+function sogd_register_settings()
+{
     //register our settings
-    register_setting( 'sogd-settings', 'sogd-festival-front-blurb' );
-    register_setting( 'sogd-settings', 'sogd-festival-enabled' );
+    register_setting('sogd-settings', 'sogd-festival-enabled');
+    register_setting('sogd-settings', 'sogd-festival-title');
+    register_setting('sogd-settings', 'sogd-festival-posts-base-cat');
+    register_setting('sogd-settings', 'sogd-festival-events-cat');
+    register_setting('sogd-settings', 'sogd-festival-front-blurb');
 }
 
-function sogd_settings_page() {
-?>
-<div class="wrap">
-<h1>Your Plugin Name</h1>
+function sogd_settings_page()
+{
+    ?>
+    <div class="wrap">
+      <h1>Strikk og Drikk</h1>
 
-<form method="post" action="options.php">
-    <?php settings_fields( 'sogd-settings' ); ?>
-    <?php do_settings_sections( 'sogd-settings' ); ?>
+      <form method="post" action="options.php">
+        <?php settings_fields('sogd-settings'); ?>
+        <?php do_settings_sections('sogd-settings'); ?>
 
-    <label>
-        <input type="checkbox" name="sogd-festival-enabled" value="<?php echo esc_attr( get_option('sogd-festival-enabled') ); ?>" />
-        Enable festival
-    </label>
-    <h2>Festival blurb</h2>
-    <?php
-        $content = get_option('sogd-festival-front-blurb');
-        wp_editor( $content, 'sogd-festival-front-blurb', $settings = array('textarea_rows'=> '10') );
-        submit_button('Save', 'primary');
-    ?>    
-</form>
-</div>
-<?php
+        <p>
+          <label>
+            <input type="checkbox" name="sogd-festival-enabled" <?php echo get_option('sogd-festival-enabled') ? 'checked' : ''; ?> />
+            Enable festival
+          </label>
+        </p>
+        <p>
+          <label>
+            Festival title
+            <input type="text" name="sogd-festival-title" value="<?php echo esc_attr(get_option('sogd-festival-title')); ?>" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Festival posts base category
+            <?php wp_dropdown_categories(array(
+                'name' => 'sogd-festival-posts-base-cat',
+                'selected' => get_option('sogd-festival-posts-base-cat'),
+                'hierarchical' => true,
+                'hide_empty' => false
+              ));
+            ?>
+          </label>
+        </p>
+        <p>
+          <label>
+            Festival events category
+            <?php wp_dropdown_categories(array(
+                'name' => 'sogd-festival-events-cat',
+                'selected' => get_option('sogd-festival-events-cat'),
+                'hide_empty' => false,
+                'taxonomy' => 'event-category'
+              ));
+            ?>
+          </label>
+        </p>
+        <h2>Festival front page blurb</h2>
+        <?php
+          $content = get_option('sogd-festival-front-blurb');
+          wp_editor(
+            $content,
+            'sogd-festival-front-blurb',
+            $settings = array(
+              'textarea_rows' => '10',
+              'teeny' => true,
+              'media_buttons' => false
+            ));
+          submit_button('Save', 'primary'); ?>
+      </form>
+    </div>
+  <?php
 }
--- theme/front-page.php
+++ theme/front-page.php
@@ -1,30 +1,43 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
+<main role="main" aria-label="Content">
+  <?php if (get_option('sogd-festival-enabled')) : ?>
     <section class="festival-hero">
-      <div
-        class="festival-hero__back"
-        style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/img/sample-festival-splash.jpg)"
-      >
+      <div class="festival-hero__back">
         <div class="layout-clamp">
           <div class="festival-hero__heading">
-            <h1>Stavanger Strikkefestival</h1>
+            <h1><?php echo esc_html(get_option('sogd-festival-title')); ?></h1>
           </div>
         </div>
       </div>
       <div class="layout-clamp">
         <div class="festival-hero__content">
-          <?php echo wp_kses_post( get_option( 'sogd-festival-front-blurb' ) ) ?>
+          <div class="festival-hero__blurb">
+            <?php echo wp_kses_post(get_option('sogd-festival-front-blurb')); ?>
+          </div>
+          <div class="festival-hero__links">
+            <ul>
+              <?php
+                $sogd_festival_posts_base_cat = get_option('sogd-festival-posts-base-cat');
+                wp_list_categories(array(
+                  'child_of' => $sogd_festival_posts_base_cat,
+                  'hide_empty' => false,
+                  'hierarchical' => false,
+                  'title_li' => null,
+                ));
+              ?>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
+  <?php endif; ?>
 
-    <section>
-      <?php get_template_part( 'loop' ); ?>
+  <section>
+    <?php get_template_part('loop'); ?>
 
-      <?php get_template_part( 'pagination' ); ?>
+    <?php get_template_part('pagination'); ?>
+  </section>
+</main>
 
-    </section>
-  </main>
-
-<?php get_footer(); ?>
+<?php get_footer();
