a08b5b6cb5b6264ed962e5d5ed66034ee1db5e9e
Nelson Menezes <nemzes@gmail.com>
2021-03-22 19:37:32 +0100

    v2.1.0

--- plugin/index.php
+++ plugin/index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Strikk og Drikk
 Description: Sets up site for Strikk og Drikk
-Version: 2.0.0
+Version: 2.1.0
 Author: Nelson Menezes
 Author URI: https://fittopage.org
 License: MIT
