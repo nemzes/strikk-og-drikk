6c7bb059ea4c8efda68bbb1bb4520439c4ffcd26
Nelson Menezes <nemzes@gmail.com>
2021-04-11 20:09:48 +0200

    Print improvements

--- theme/footer.php
+++ theme/footer.php
@@ -4,11 +4,14 @@
           <p class="copyright">
             &copy; <?php echo esc_html(date('Y')); ?> Copyright <?php bloginfo('name'); ?>
           </p>
+          
+          <div class="ssod-footer__widgets">
+            <?php if (is_active_sidebar('ssod-widgets-footer')) : ?>
+              <?php dynamic_sidebar('ssod-widgets-footer'); ?>
+            <?php endif; ?>
+            <?php wp_footer(); ?>
+          </div>
 
-          <?php if (is_active_sidebar('ssod-widgets-footer')) : ?>
-            <?php dynamic_sidebar('ssod-widgets-footer'); ?>
-          <?php endif; ?>
-          <?php wp_footer(); ?>
         </div>
         <a href="/wp-admin" class="ssod-footer__pi">&pi;</a>
       </footer>
